1a65225fca38f716d7e3f1bbb38c23d8dacf880e
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-07-18 08:28:42 -0600

    Added a componentDidUpdate to rerender and send athoer get request when the component rerenders.  It was neccessary to include an if statement to make sure the the previous prop that was the get request was not the same otherwise it would just create an infinte loop of requests

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import axios from 'axios';
 
 class ResourceList extends React.Component {
+  state = { resources: []}
+
+  async componentDidMount() {
+    const response = await axios.get(`http://jsonplaceholder.typicode.com/${this.props.resource}`)
+    // console.log("response", response)
+    this.setState({resources: response.data})
+  }
+
+  async componentDidUpdate(prev) {
+    if (prev.resource !== this.props.resource) {
+      const response = await axios.get(`http://jsonplaceholder.typicode.com/${this.props.resource}`)
+      this.setState({ resources: response.data })
+    }
+  }
+
   render() {
     return (
       <div>
-        {this.props.resource}
+        {this.state.resources.length}
       </div>
     )
   }

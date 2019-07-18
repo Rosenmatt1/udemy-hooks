afcb47312938d9f40489505c26f123e407ba3ccb
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-07-17 22:56:39 -0600

    Created class based App with basic state and functions

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,19 @@
-import react
+import React from 'react'
+
+class App extends React.Component {
+  state = { resource: 'posts '};
+
+  render() {
+    return (
+      <div>
+        <div> 
+          <button onClick={() => this.setState({resource: 'posts'})}>Posts</button>
+          <button onClick={() => this.setState({ resource: 'todos' })}>Todos</button>
+        </div>
+        {this.state.resource}
+      </div>
+    )
+  }
+}
+
+export default App

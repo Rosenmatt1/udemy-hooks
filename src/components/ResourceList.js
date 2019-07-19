339c072ea78b8ed366e84197d1200f9184ddfb98
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-07-18 18:03:41 -0600

    Added array as second argument which will trigger useEffect if changed

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,15 +1,18 @@
 import React, {useState, useEffect} from 'react'
-import axios from 'axios';
+import axios from 'axios'
 
-const ResourceList = () => {
+const ResourceList = ( {resource} ) => {
   const [resources, setResources] = useState([])
 
-  
-
-  const fetchResource = async () => {
-    const response = await axios.get(`http://jsonplaceholder.typicode.com/${this.props.resource}`)
+  const fetchResource = async (resource) => {
+    const response = await axios.get(`http://jsonplaceholder.typicode.com/${resource}`)
     // console.log("response", response)
+    setResources(response.data)
   }
+
+  useEffect(() => {
+    fetchResource(resource)
+  }, [resource])
 
     return (
       <div>

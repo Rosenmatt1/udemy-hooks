22eefe4a82de47279dd0557ac0eba95eb8416563
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-07-19 11:10:38 -0600

    Rendered the list

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -26,9 +26,10 @@ const ResourceList = ({ resource }) => {
   )
 
   return (
-    <div>
-      {resources.length}
-    </div>
+    <ul>
+      {resources.map(record => <li key={record.id}> {record.title} </li>)}
+      
+    </ul>
   )
 }
 

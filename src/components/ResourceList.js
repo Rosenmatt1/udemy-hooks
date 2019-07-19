bca422bd7903a0a5bbcf9da26bb833ed8d23a573
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-07-18 18:41:10 -0600

    Created a new useEffect function where the async function is wrapped in parenthesis and immediatly invoked.  This allows an async function to be immediatly invoked

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,24 +1,35 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const ResourceList = ( {resource} ) => {
+const ResourceList = ({ resource }) => {
   const [resources, setResources] = useState([])
 
-  const fetchResource = async (resource) => {
-    const response = await axios.get(`http://jsonplaceholder.typicode.com/${resource}`)
-    // console.log("response", response)
-    setResources(response.data)
-  }
+  // const fetchResource = async (resource) => {
+  //   const response = await axios.get(`http://jsonplaceholder.typicode.com/${resource}`)
+  //   // console.log("response", response)
+  //   setResources(response.data)
+  // }
 
-  useEffect(() => {
-    fetchResource(resource)
-  }, [resource])
+  // useEffect(() => {
+  //   fetchResource(resource)
+  // }, [resource])
 
-    return (
-      <div>
-        {resources.length}
-      </div>
-    )
+
+  useEffect(
+    () => {
+      (async resource => {
+        const response = await axios.get(`http://jsonplaceholder.typicode.com/${resource}`)
+        // console.log("response", response)
+        setResources(response.data)
+      })(resource)
+    }, [resource]
+  )
+
+  return (
+    <div>
+      {resources.length}
+    </div>
+  )
 }
 
 export default ResourceList
